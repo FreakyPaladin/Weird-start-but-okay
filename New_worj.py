9bf955c2aaaa2913883de95b2237e7c405f3443a
Lawrence Eshun <leseshun@st.ug.edu.gh>
2026-07-28 19:30:57 +0000

    new something. I'll be more dedicated

---
+++ New_worj.py
@@ -0,0 +1,15 @@
+price =10
+price=20 
+print(price)
+
+
+
+
+name= "John Smith"
+age= 20
+is_new= True
+
+name=input('What is your name?')
+color=input('What is your favorite color?')
+print(name,"likes",color)
+print(name + ' likes ' + color)
---
+++ newbie.py
@@ -0,0 +1,14 @@
+#Write a program that stores:
+
+#Your name
+#Your age
+#Your favorite programming language
+
+
+name=input('What is your name?')
+age=input("What is your age?")
+lang=input("What is your favourite programming language?")
+
+print("name: ", name)
+print("age: ", age)
+print("Favourite programming language ", lang)
